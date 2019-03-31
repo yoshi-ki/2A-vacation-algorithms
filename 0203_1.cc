2f97e2bd51f9a36a26cd9aaf3ee392296d05a637
yoshi-ki <yoshikif98@gmail.com>
2019-03-31 23:42:18 +0900

    0203 ABC contest

---
+++ 0203_1.cc
@@ -0,0 +1,12 @@
+//https://atcoder.jp/contests/abc117/tasks/abc117_a
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  float T,X;
+  cin >> T >> X;
+  cout << T/X << endl;
+  return 0;
+}
